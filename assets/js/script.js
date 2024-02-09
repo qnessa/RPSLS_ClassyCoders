57546a317eca9b99433f1d9a289925ff027ad18f
steve whitehouse <stewhites@blueyonder.co.uk>
2024-02-09 01:43:47 +0000

    javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 //Declare variables
-let playerName = "Player";
 let playerTurn = "";
 let computerTurn = "";
 let randNum = "";
 let scores = [0, 0, 0];
-let playerWin = "";
 let roundEnded = false
 //let clickedPlayButton = document.getElementById('');
 let messagePlayer = document.getElementById("messageplayer");
 let playerChoiceId = ''
-let result ='';
+let result = '';
+let delay = 1000
+const throttledResetGame = throttle(resetGame, delay);
 let playerChoiceIds = [
   "playerChoice-rock",
   "playerChoice-paper",
@@ -17,70 +17,95 @@ let playerChoiceIds = [
   "playerChoice-lizard",
   "playerChoice-spock",
 ];
-//**const need score dom variables**
 //Let the DOM Load
 document.addEventListener("DOMContentLoaded", function () {});
-//get players choice from DOM
+
+//get players choice from DOM (main loop)
+
 function handleButtonClick() {
+  document.getElementById('play').addEventListener("click", function () {
+    console.log("Start button clicked")
+  });
+  document.getElementById('reset').addEventListener("click", function () {
+    location.reload()
+  });
   playerChoiceIds.forEach(function (playerChoice) {
     playerChoiceId = document.getElementById(playerChoice);
     if (playerChoiceId) {
       playerChoiceId.addEventListener("click", function () {
         if (!roundEnded) {
           playerTurn = playerChoice.slice(13);
-          console.log(`player has chosen ${playerTurn}`)
-          let computerTurn = getComputerTurn();
-          getResult(playerTurn, computerTurn, scores);
+          let computerTurn = getComputerTurn(roundEnded);
+          getResult(playerTurn, computerTurn);
         }
         roundEnded = true;
-        resetGame(scores)
+        throttledResetGame(scores)
       });
     }
   });
 }
 // Generate Computer Turn
-function getComputerTurn() {
+function getComputerTurn(roundEnded) {
   var turns = ["rock", "paper", "scissors", "lizard", "spock"];
-  console.log(turns);
   randNum = Math.floor(Math.random() * 5);
   computerTurn = turns[randNum];
-  console.log(`computer has chosen ${computerTurn}`)
   return computerTurn;
 }
 //get result comparison
-function getResult() {
-  console.log("getResult is called")
-  console.log(playerTurn)
-  console.log(computerTurn)
-  console.log(`scores are ${scores}`)
+function getResult(roundEnded) {
+
   if (playerTurn === computerTurn) {
     messagePlayer.innerText = "It's a draw";
-    result= "draw"
-  } else if ((playerTurn === 'rock' && (computerTurn === 'scissors' || computerTurn === 'lizard')) || (playerTurn === 'paper' && (computerTurn === 'rock' || computerTurn === 'spock')) ||
+    result = "draw"
+  } else if ((playerTurn === 'rock' && (computerTurn === 'scissors' || computerTurn === 'lizard')) ||
+    (playerTurn === 'paper' && (computerTurn === 'rock' || computerTurn === 'spock')) ||
     (playerTurn === 'scissors' && (computerTurn === 'paper' || computerTurn === 'lizard')) ||
     (playerTurn === 'lizard' && (computerTurn === 'spock' || computerTurn === 'paper')) ||
     (playerTurn === 'spock' && (computerTurn === 'scissors' || computerTurn === 'rock'))) {
-    messagePlayer.innerText = `${playerTurn} beats ${computerTurn}`;
-    result ="win"
+    messagePlayer.innerText = `You Win, ${playerTurn} beats ${computerTurn}`;
+    result = "win"
   } else if (playerTurn == false || computerTurn == false) {
     messagePlayer.innerText = "Make your Choice below"
   } else {
-    messagePlayer.innerText = `${computerTurn} beats ${playerTurn}`;
+    messagePlayer.innerText = `You Lose, ${computerTurn} beats ${playerTurn}`;
     result = "loss"
   }
+
   return result
 }
 //reset round
 function resetGame() {
+  console.log(result);
   playerChoice = null;
   computerChoice = null;
-  if (result ==="win") {scores[0] +=1
-  } else if (result ==="draw") {scores[1] +=1}
-  else if (result==="loss") {scores[2] +=1 }
+  if (result == "win") {
+    scores[0] += 1
+  } else if (result == "draw") {
+    scores[1] += 1
+  } else if (result == "loss") {
+    scores[2] += 1
+  }
   result = '';
-  document.getElementById('score-won').innerText =scores[0];
-  document.getElementById('score-drew').innerText =scores[1];
-  document.getElementById('score-lost').innerText =scores[2];
+  document.getElementById('score-won').innerText = scores[0];
+  document.getElementById('score-drew').innerText = scores[1];
+  document.getElementById('score-lost').innerText = scores[2];
   roundEnded = false;
+  throttle();
+}
+
+function throttle(cb, delay) {
+  let wait = false;
+
+  return (...args) => {
+    if (wait) {
+      return;
+    }
+
+    cb(...args);
+    wait = true;
+    setTimeout(() => {
+      wait = false;
+    }, delay);
+  }
 }
 handleButtonClick()
